1ffc13fd9e98b303a0a914175b6503c95c7b06b1
OSCAR EDUARDO <oscareduardorodriguezcastro3@gmail.com>
2020-09-03 22:51:59 -0500

    tercer commit

--- src/controller/Admin.controller.js
+++ src/controller/Admin.controller.js
@@ -1,7 +1,7 @@
-const firebase = require('../configfirebase');
+// const firebase = require('../configfirebase');
 
 const controlador = {};
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
 controlador.inicio = (req, res) => {
     res.render('index');
@@ -23,45 +23,45 @@ controlador.guardar = (req,res)=>{
 
 module.exports = controlador;
 
-function agregardatos() {
-    // leerDatos();
-    db.collection("perfil").add({
-        nombre: txnombres.value,
-        telefono: txtelefono.value,
-        correo: txcorreo.value,
+// function agregardatos() {
+//     // leerDatos();
+//     db.collection("perfil").add({
+//         nombre: txnombres.value,
+//         telefono: txtelefono.value,
+//         correo: txcorreo.value,
         
-    })
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-            alert('datos agregados correctamennte', docRef.id);
-        })
-        .catch((error) => {
-            console.error("Error: ", error);
-        });
-}
+//     })
+//         .then((docRef) => {
+//             console.log("Document written with ID: ", docRef.id);
+//             alert('datos agregados correctamennte', docRef.id);
+//         })
+//         .catch((error) => {
+//             console.error("Error: ", error);
+//         });
+// }
 
-function leerDatos() {
-    listaagendas.innerHTML = "";
-    // btnActualizar.classList.add('d-none');
+// function leerDatos() {
+//     listaagendas.innerHTML = "";
+//     // btnActualizar.classList.add('d-none');
 
-    db.collection("agendas").get()
-        .then((querySnapshot) => {
-            // esquema para definir funciones anonimas
-            querySnapshot.forEach((doc) => {
-                listaagendas.innerHTML += `
-                   <tr>
-                       <td>${doc.data().nombre}</td>
-                       <td>${doc.data().telefono}</td>
-                       <td>${doc.data().correo}</td>
-                       <td>
-                           <button onclick="eliminar('${doc.id}')" class="btn btn-danger">delete</button>
+//     db.collection("agendas").get()
+//         .then((querySnapshot) => {
+//             // esquema para definir funciones anonimas
+//             querySnapshot.forEach((doc) => {
+//                 listaagendas.innerHTML += `
+//                    <tr>
+//                        <td>${doc.data().nombre}</td>
+//                        <td>${doc.data().telefono}</td>
+//                        <td>${doc.data().correo}</td>
+//                        <td>
+//                            <button onclick="eliminar('${doc.id}')" class="btn btn-danger">delete</button>
                        
-                       </td>
-                   </tr>           
-               `;
-            });
-        })
-        .catch(function (error) {
-            console.log("Error: ", error);
-        });
-}
+//                        </td>
+//                    </tr>           
+//                `;
+//             });
+//         })
+//         .catch(function (error) {
+//             console.log("Error: ", error);
+//         });
+// }
